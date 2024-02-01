6b42dd78b7155d1a945c9d1b5e85acdcd3dcbaed
KarthikaMovva <karthika.movva@kalvium.community>
2024-02-01 17:03:46 +0530

    added

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,6 +8,29 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 // complete the function below:
 function Carousel() {
+    const[number,setnumber]=useState(0);
+
+    function Handleleftarrow(){
+        setnumber((prevnumber)=>(prevnumber===0? images.length-1 : prevnumber-1))
+    }
+
+   function Handlerightarrow(){
+        setnumber((prevnumber)=>(prevnumber===images.length-1? 0 : prevnumber+1))
+    }
+
+    const presentimg = images[number]
+
+    return(
+        <div className="total">
+    <div className="arrange">
+        <div className="title">{presentimg.title}</div>
+        <div className="subtitle">{presentimg.subtitle}</div>
+    </div>
+        <ArrowBackIosIcon onClick={Handleleftarrow} className="first arrow"/>
+        <img src={presentimg.img} alt="" className="image"/>
+        <ArrowForwardIosIcon onClick={Handlerightarrow} className="second arrow"/>
+        </div>
+    )
 }
 
 export default Carousel;
